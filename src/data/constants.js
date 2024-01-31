ddbc9833aae8f7dca90e5dc8b3dffe1eb351a62d
VUDUMULA NAGA SAI RAHUL <110412514+Rahul21sai@users.noreply.github.com>
2024-01-31 12:18:55 +0530

    constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Rahul21sai",
   resume:
-    "https://drive.google.com/file/d/1JwSJ28I3QwVppk5lC8SDmc5PNNXF9nDq/view?usp=sharing",
+    "https://drive.google.com/file/d/1mbJF7sc_ML1mV7EK1XNKoLxBpW4mLT2E/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/naga-sai-rahul-vudumula-93419524b",
   twitter: "",
   insta: "https://instagram.com/_naga_sai_rahul_?igshid=MzMyNGUyNmU2YQ==",
